561f8075959a05e54bf7a16f1080b54f0d7915d2
Anthony Miller <1anthony1@gmail.com>
2018-12-12 22:58:30 -0700

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,6 @@ app.engine("handlebars", exphbs({
 app.set("view engine", "handlebars");
 
 // Database configuration with mongoose
-//mongoose.connect("mongodb://heroku_jmv816f9:5j1nd4taq42hi29bfm5hobeujd@ds133192.mlab.com:33192/heroku_jmv816f9");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mongoscraper");
 const db = mongoose.connection;
 
